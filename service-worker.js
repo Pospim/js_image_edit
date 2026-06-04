a8a390325597ab6ae39d4ca6edfdb0bb31eb117d
Pospim <pospim.inf@gmail.com>
2026-06-04 08:58:30 +0200

    mobile app

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "image-edit-v1";
+
+const FILES = [
+	"./",
+	"./image_edit.html",
+	"./manifest.webmanifest",
+	"./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+	);
+});
+
+self.addEventListener("activate", (event) => {
+	event.waitUntil(
+		caches.keys().then((names) => {
+			return Promise.all(
+				names
+					.filter((name) => name !== CACHE_NAME)
+					.map((name) => caches.delete(name))
+			);
+		})
+	);
+});
+
+self.addEventListener("fetch", (event) => {
+	if (event.request.method !== "GET") {
+		return;
+	}
+
+	event.respondWith(
+		caches.match(event.request).then((cached) => {
+			return cached || fetch(event.request);
+		})
+	);
+});
